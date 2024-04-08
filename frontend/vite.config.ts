4f96b314f8a9e1201907a23996c63b064c85b418
Ash <809189656@qq.com>
2024-04-08 14:05:39 +0800

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
 
 	server: {
 		host: '127.0.0.1',
-		port: 1224,
+		port: 34115,
 	},
 })
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"os/exec"
+	"path/filepath"
+	"syscall"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -12,14 +15,22 @@ import (
 var assets embed.FS
 
 func main() {
+	// 调用 deepzoom server 服务
+	relativePath := "./dzi-server/dist/main.exe"
+	absPath, absPathErr := filepath.Abs(relativePath)
+	if absPathErr != nil {
+		panic(absPathErr)
+	}
+	cmd := exec.Command("cmd", "/C", absPath)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.Start()
 	// Create an instance of the app structure
 	app := NewApp()
-
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "dzishop",
-		Width:  1024,
-		Height: 768,
+		Width:  1280,
+		Height: 720,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
@@ -30,7 +41,6 @@ func main() {
 		},
 		Frameless: true,
 	})
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
